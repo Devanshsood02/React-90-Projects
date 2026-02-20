53e77580879661dfd923b61bfe960b99e108718a
Devansh Sood <sooddevansh143.gmail.com>
2026-02-20 23:54:59 +0530

    StopTimer made

--- 3project/src/App.jsx
+++ 3project/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
+
+const App = () => {
+  
+  const [iscounting, setIsCounting] = useState(false);
   const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(()=>{
+    if(!iscounting) return
+    console.log(count)
 
-export default App
+   let interval= setInterval(() => {
+
+      setCount((prev)=>prev+1)
+      
+    }, 1000);
+
+
+    return ()=>{
+       clearInterval(interval)
+    }
+  },[iscounting])
+  return (
+    <div className="bg-gray-800 min-h-screen flex items-center justify-center">
+      <div className="h-70 w-70 rounded-full  border-gray-400 border-4 text-white flex flex-col items-center justify-evenly">
+
+
+
+        <h1 className="text-red-400 text-5xl">{count}</h1>
+        <button className="text-3xl border px-8 py-2 rounded-xl cursor-pointer" onClick={
+          ()=>{
+            setIsCounting((prev)=>!prev)
+          }
+          
+        }> {iscounting?"Stop":"Start"}</button>
+
+        <button className="text-3xl border px-8 py-2 rounded-xl cursor-pointer" onClick={()=>{
+          setCount(0)
+          setIsCounting(false)
+        }}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
