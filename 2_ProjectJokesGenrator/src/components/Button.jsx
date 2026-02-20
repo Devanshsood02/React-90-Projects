0dd5c8ac5213ca81cb2c66682636d682d5eae57d
Devansh Sood <sooddevansh143.gmail.com>
2026-02-20 19:57:02 +0530

    3rd project

--- 2_ProjectJokesGenrator/src/components/Button.jsx
+++ 2_ProjectJokesGenrator/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({jokecalling}) => {
   return (
     <div>
-        <button className='bg-[#598e1d] hover:bg-[#5bb407] active:bg-[#297910] text-white text-xl px-12 py-3 rounded-xl cursor-pointer' onClick={jokecalling}>Click Here </button>
+        <button className='bg-[#598e1d] hover:bg-[#5bb407] active:bg-[#297910] text-white text-xl px-12 py-3 rounded-xl cursor-pointer2 ' onClick={jokecalling}>Click Here </button>
     </div>
   )
 }
