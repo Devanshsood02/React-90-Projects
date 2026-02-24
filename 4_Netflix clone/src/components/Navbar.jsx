103553b61f40510b72f40833ce3e2baba90e06cb
Devansh Sood <sooddevansh143@gmail.com>
2026-02-24 08:04:51 +0530

    Navbar completed

--- 4_Netflix clone/src/components/Navbar.jsx
+++ 4_Netflix clone/src/components/Navbar.jsx
@@ -5,10 +5,9 @@ import bell_icon from "../assets/bell_icon.svg";
 import profile_img from "../assets/profile_img.png";
 import caret_icon from "../assets/caret_icon.svg";
 
-
 const Navbar = () => {
   return (
-    <nav className=" w-full bg-green-500  h-14 flex  justify-between position:fixed text-xl p-7 text-[#e5e5e5] bg-[linear-gradient(180deg,rgba(0,0,0,0.7)_10%,transparent)] z-1">
+    <nav className=" w-full bg-green-600  h-14 flex  justify-between position:fixed text-xl p-7 text-[#e5e5e5] bg-[linear-gradient(180deg,rgba(0,0,0,0.7)_10%,transparent)] z-1 font-poppins">
       <div className="left  flex items-center gap-12">
         <figure className="w-20">
           <img src={logo} alt="" />
@@ -21,28 +20,30 @@ const Navbar = () => {
           <li>New& Popular</li>
           <li>My List</li>
           <li>Browse by Language</li>
-
-          
-            </ul>
+        </ul>
       </div>
 
-      <div className="right ">
-        <figure><img src={search_icon} alt="" /></figure>
+      <div className="right flex gap-7 items-center text-m">
+        <figure>
+          <img src={search_icon} alt="" />
+        </figure>
         <p>children</p>
-        <figure><img src={bell_icon} alt="" /></figure>
+        <figure>
+          <img src={bell_icon} alt="" />
+        </figure>
 
-        <div className="navbar-profile">
-        <figure><img src={profile_img} alt="" /></figure>
-        <figure><img src={caret_icon} alt="" /></figure>
+        <div className="navbar-profile flex items-center w-29 gap-2 cursor-pointer position:relative  ">
+          <figure>
+            <img src={profile_img} alt="" />
+          </figure>
+          <figure>
+            <img src={caret_icon} alt="" />
+          </figure>
 
-
-
-
-
-
-
+         <div className="absolute top-full right-0 w-max bg-[#191919] px-[22px] py-[18px] rounded-[2px] underline z-1">
+            <p className="text-xl cursor-pointer">Sign out of Netflix</p>
+          </div>
         </div>
-
       </div>
     </nav>
   );
