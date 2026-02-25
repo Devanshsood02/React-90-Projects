8dee4f15a8784096663bb5dba5e43117e215e3a6
Devansh Sood <sooddevansh143@gmail.com>
2026-02-25 20:40:35 +0530

    Implement slightly new method of routing (but still old)

--- 5_react router greatStack/src/App.jsx
+++ 5_react router greatStack/src/App.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import { Routes,Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Products from './Pages/Products'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import {
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Products from "./Pages/Products";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import RootLayout from "./layout/RootLayout";
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className='container'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        
-         </Routes>
-         </div>
-    </div>
-  )
-}
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>,
+    ),
+  );
 
-export default App
+  return (
+
+
+<RouterProvider router={router}/>
+
+
+    // <div>
+    //    <Navbar />
+    //  <div className='container'>
+    //   <Routes>
+    //     <Route path='/' element={<Home />} />
+    //     <Route path='/products' element={<Products />} />
+    //     <Route path='/about' element={<About />} />
+    //     <Route path='/contact' element={<Contact />} />
+        
+    //      </Routes>
+    //      </div> 
+
+    // </div> 
+    //   this was the old of routing 
+  );
+};
+
+export default App;
